64e56df90dda6b84680ee3b39c629d833aa10c92
Saaim-K <saaimkhan221@gmail.com>
2023-01-11 02:54:59 +0500

    Single File Upload Succesful

--- server.mjs
+++ server.mjs
@@ -4,12 +4,28 @@ import multer from 'multer'
 const app = express()
 const port = process.env.PORT || 4444
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/upload', (req, res) => {
-    res.send('Image Uploaded!')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'Upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage }).array('image')
+
+app.post('/upload', upload, (req, res) => {
+    res.send('File Uploaded!')
+    console.log(req.file)
+    if (!req.file) {
+        res.status(400).send("Image not uploaded")
+    }
 })
 
 app.listen(port, () => {
